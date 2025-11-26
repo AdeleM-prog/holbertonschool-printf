129db9a749869cdb16d3fab97c02335e581c73ec
AdeleM-prog <12043@holbertonstudents.com>
2025-11-26 12:36:06 +0000

    declaration des fonction pointees, utilisees dans la fonction _printf

--- main.h
+++ main.h
@@ -1,7 +1,12 @@
-#ifndef PRINTF_H
-#define PRINTF_H
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
 
 int _printf(const char *format, ...);
-
+int print_char(va_list args);
+int print_string(va_list args);
+int print_percent(va_list args);
+int print_int(va_list args);
 
 #endif
