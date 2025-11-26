73198425c024dcf23626655137c15ac860375223
AdeleM-prog <12043@holbertonstudents.com>
2025-11-26 15:21:37 +0000

    Correction : - ajout tableau de structure (specificateurs connus) - ajout pointeur de fonctions - gestion du specificateur de conversion inconnu

---
+++ _printf.c
@@ -0,0 +1,62 @@
+#include <stdarg.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ * _printf - Produces output according to a format
+ * @format format to print
+ *
+ * Return: Number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int i = 0;
+	int nbChar = 0;
+	char *str;
+	char c;
+	int num;
+	int j;
+	
+struct spec table[] =
+  {
+    {'c', print_char},
+    {'s', print_string},
+    {'%', print_percent},
+    {'d', print_int},
+    {'i', print_int},
+    {'\0', NULL}
+  };
+    
+	va_start(args, format);
+	if (format == NULL)
+	{
+		return (-1);
+	}
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+		  for (j = 0; table[j] != '0'; j++)
+		    {
+		      if (format[i] == table[j].spec)
+			{
+			 nbChar = nbChar + table[j].f(args);
+			}
+		    }
+		  if (table[j].spec == '\0')
+		    {
+		     write(1, "%", 1);
+		     write(1, &format[i], 1);
+		     nbChar = nbChar + 2;
+		     }
+		}
+		else
+		{
+			write(1, &format[i], 1);
+			nbChar++;
+		}
+		i++;
+	}
+	va_end(args);
+	return (nbChar);
+}
