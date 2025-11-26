e7849f779a9f8d77f6ff05e585209c4b2a81d046
laetitiagrondin <laetitiagrondin93@gmail.com>
2025-11-26 18:11:12 +0000

    - Creation du fichier spec.c - Inclusion des bibliotheques : -> stddef.h : pour NULL -> unistd.h : pour la fonction write() - Ajout des fonctions : -> print_char : affiche un caractere -> print_string : affiche une chaine de caracteres -> print_percent : affiche un pourcentage -> print_int : affiche un entier

---
+++ spec.c
@@ -0,0 +1,126 @@
+#include <stddef.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_char - Prints a single character
+ * @args: a list of arguments,
+ * from which the character to print is extracted
+ *
+ * Return: 1, representing the number of characters printed
+ */
+int print_char(va_list args)
+{
+	char c;
+
+	c = va_arg(args, int);
+	write(1, &c, 1);
+	return (1);
+}
+
+/**
+ * print_string - Prints a string
+ * @args: a list of arguments,
+ * from which the string to print is extracted
+ *
+ * Return: The number of characters printed
+ */
+int print_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+	int i = 0;
+	int len = 0;
+
+	if (str == NULL)
+	{
+		str = "(null)";
+	}
+	while (str[i] != '\0')
+	{
+		write(1, str, 1);
+		len++;
+		i++;
+	}
+	return (len);
+}
+
+/**
+ * print_percent - Prints a percent symbol
+ * @args: unused, as no argument
+ * is needed to print '%'
+ *
+ * Return: 1, representing the number of characters printed
+ */
+int print_int(va_list args)
+{
+	int num;
+	int len = 0;
+	int divisor = 1;
+	char c;
+	
+	num = va_arg(args, int);
+	if (num == 0)
+	{
+		write(1, "0", 1);
+		return (1);
+	}
+	if (num < 0)
+	{
+		write(1, "-", 1);
+		num = -num;
+		len++;
+	}
+	while (num / divisor >= 10)
+	{
+		divisor *= 10;
+	}
+	while (divisor > 0)
+	{
+		c = (num / divisor) + '0';
+		write(1, &c, 1);
+		len++;
+		num %= divisor;
+		divisor /= 10;
+	}
+	return (len);
+}
+
+/**
+ * print_int - Prints an integer to the standard output
+ * @args: a list of arguments containing the integer to be printed
+ *
+ * Return: The number of characters printed
+ */
+int print_int(va_list args)
+{
+	int num;
+	int len = 0;
+	char c;
+	divisor = 1;
+
+	num = va_arg(args, int);
+	if (num == 0)
+	{
+		write(1, "0", 1);
+		return (1);
+	}
+	if (num < 0)
+	{
+		write(1, "-", 1);
+		num = -num;
+		len++;
+	}
+	while (num / divisor >= 10)
+	{
+		divisor *= 10;
+	}
+	while (divisor > 0)
+	{
+		c = (num / divisor) + '0';
+		write(1, &c, 1);
+		len++;
+		num %= divisor;
+		divisor /= 10;
+	}
+	return (len);
+}
